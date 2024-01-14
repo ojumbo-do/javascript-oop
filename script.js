536ca95385b5cb3a4f264b89ea33007d74785845
Dancun Ojumbo <70913596+klaus990@users.noreply.github.com>
2024-01-14 18:35:01 +0300

    Implementing classes in ES6

--- script.js
+++ script.js
@@ -16,3 +16,54 @@ Person.prototype.calcAge = function () {
 };
 
 dan.calcAge();
+
+//Coding Challenge in constructor functions
+const Car = function (make, speed) {
+  this.make = make;
+  this.speed = speed;
+};
+
+const bmw = new Car('BMW', 120);
+const mercedes = new Car('Mercedes', 95);
+
+Car.prototype.accelerate = function () {
+  console.log(`${(this.speed += 10)}km/h`);
+};
+
+Car.prototype.brake = function () {
+  console.log(`${(this.speed -= 5)}km/h`);
+};
+
+bmw.accelerate();
+mercedes.accelerate();
+
+bmw.brake();
+mercedes.brake();
+
+////////////////////////////////////
+// Implementing classes in es6
+
+//class expression
+// const PersonCL = class{}
+
+//class declaration
+class PersonCL {
+  constructor(firstName, birthYear) {
+    this.firstName = firstName;
+    this.birthYear = birthYear;
+  }
+
+  //Methods will be added to .prototype property
+  calcAge() {
+    console.log(2040 - this.birthYear);
+  }
+
+  greet() {
+    console.log(`Hi! ${this.firstName}`);
+  }
+}
+
+const jessica = new PersonCL('Jessica', 1997);
+
+console.log(jessica);
+jessica.greet();
