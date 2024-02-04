17f1af371110f29c45c981a28138050e0eda6dd1
Dancun Ojumbo <70913596+klaus990@users.noreply.github.com>
2024-02-04 13:01:13 +0300

    Challenge #4

--- script.js
+++ script.js
@@ -221,40 +221,40 @@ brenda.introduce();
 brenda.calcAge();
 
 // challenge 03
-const Car = function (make, speed) {
-  (this.make = make), (this.speed = speed);
-};
+// const Car = function (make, speed) {
+//   (this.make = make), (this.speed = speed);
+// };
 
-Car.prototype.brake = function () {
-  this.speed -= 5;
-  console.log(`${this.make} is going at ${this.speed}km/h`);
-};
+// Car.prototype.brake = function () {
+//   this.speed -= 5;
+//   console.log(`${this.make} is going at ${this.speed}km/h`);
+// };
 
-const EV = function (make, speed, charge) {
-  Car.call(this, make, speed), (this.charge = charge);
-};
+// const EV = function (make, speed, charge) {
+//   Car.call(this, make, speed), (this.charge = charge);
+// };
 
-EV.prototype = Object.create(Car.prototype);
+// EV.prototype = Object.create(Car.prototype);
 
-EV.prototype.chargeBattery = function (chargeTo) {
-  this.charge = chargeTo;
-  console.log(`${this.make} is ${this.charge}% charged`);
-};
+// EV.prototype.chargeBattery = function (chargeTo) {
+//   this.charge = chargeTo;
+//   console.log(`${this.make} is ${this.charge}% charged`);
+// };
 
-EV.prototype.accelerate = function () {
-  this.speed += 20;
-  this.charge--;
+// EV.prototype.accelerate = function () {
+//   this.speed += 20;
+//   this.charge--;
 
-  console.log(
-    `${this.make} going at ${this.speed} km/h, with charge of ${this.charge}%`
-  );
-};
+//   console.log(
+//     `${this.make} going at ${this.speed} km/h, with charge of ${this.charge}%`
+//   );
+// };
 
-const tesla = new EV('Tesla', 120, 23);
+// const tesla = new EV('Tesla', 120, 23);
 
-tesla.chargeBattery(80);
-tesla.brake();
-tesla.accelerate();
+// tesla.chargeBattery(80);
+// tesla.brake();
+// tesla.accelerate();
 
 //Inheritance in ES6 CLasses
 class PersonCL {
@@ -376,16 +376,19 @@ class Account {
 
   deposit(value) {
     this.#movements.push(value);
+    return this;
   }
 
   withdraw(value) {
     this.deposit(-value);
+    return this;
   }
 
   requestLoan(value) {
     if (this.#approveLoan(value)) {
       this.deposit(value);
       console.log('Loan approved');
+      return this;
     }
   }
 
@@ -405,3 +408,52 @@ console.log(...acc1.getMovements());
 // console.log(acc1.#movements);
 
 //Data privacy and encapsulation- private class fields and methods
+
+//methods chaining - add return this to the individual methods
+acc1.deposit(1000).deposit(200).requestLoan(30000).withdraw(25000);
+console.log(acc1.getMovements());
+
+//challenge #4
+
+class CarCl {
+  constructor(make, speed) {
+    this.make = make;
+    this.speed = speed;
+  }
+  brake() {
+    this.speed -= 5;
+    console.log(`${this.make} is going at ${this.speed}km/h`);
+    return this;
+  }
+}
+
+class EVCl extends CarCl {
+  #charge;
+
+  constructor(make, speed, charge) {
+    super(make, speed);
+    this.#charge = charge;
+  }
+  chargeBattery(chargeTo) {
+    this.#charge = chargeTo;
+    console.log(`${this.make} is ${this.#charge}% charged`);
+    return this;
+  }
+  accelerate() {
+    this.speed += 20;
+    this.#charge--;
+
+    console.log(
+      `${this.make} going at ${this.speed} km/h, with charge of ${
+        this.#charge
+      }%`
+    );
+    return this;
+  }
+}
+
+const rivian = new EVCl('Rivian', 120, 23);
+
+rivian.chargeBattery(80).brake().accelerate();
+
+console.log(rivian);
